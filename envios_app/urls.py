c1487ca34cf8e8f9f87fd7a91f073a335c6d7ed3
Daniel-615 <suyanangel615@gmail.com>
2024-10-23 14:07:56 -0600

    feature vehiculos

--- envios_app/urls.py
+++ envios_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import actualizar_asignacion,crear_asignacion
+from .views import actualizar_asignacion,crear_asignacion,listar_asignaciones,listar_vehiculos,crear_vehiculo,actualizar_vehiculo
 
 urlpatterns = [
     path('asignaciones/editar/<uuid:id>/', actualizar_asignacion, name='actualizar_asignacion'),
-    path('asignaciones/crear/',crear_asignacion,name='crear_asignacion')
+    path('asignaciones/crear/',crear_asignacion,name='crear_asignacion'),
+    path('asignaciones/listar/',listar_asignaciones,name='listar_asignaciones'),
+    path('vehiculo/editar/<uuid:id>/',actualizar_vehiculo,name='actualizar_vehiculo'),
+    path('vehiculo/crear/',crear_vehiculo,name='crear_vehiculo'),
+    path('vehiculo/listar',listar_vehiculos,name='listar_vehiculos'),
 ]
--- envios_app/views.py
+++ envios_app/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from .models import Cliente,Vehiculo,Facturacion,FacturacionDetalle,Rastreo,Ubicacion,Transportista,Envio,Paquete,AsignacionVehiculoTransportista as Asignacion 
+def listar_vehiculos(request):
+    vehiculos = Vehiculo.objects.all()  
+    return render(request, 'asignacion/listar_vehiculos.html', {'vehiculos': vehiculos})
+
+def crear_vehiculo(request):
+    if request.method=='GET':
+        return render(request,'asignacion/create_vehiculo.html')
+    if request.method=='POST':
+        matricula_vehiculo=request.POST.get('matricula')
+        marca_vehiculo=request.POST.get('marca')
+        modelo_vehiculo=request.POST.get('modelo')
+
+        nuevo_vehiculo= Vehiculo(
+            matricula_vehiculo,
+            marca_vehiculo,
+            modelo_vehiculo
+        )
+        nuevo_vehiculo.save()
+
+        return redirect('listar_vehiculos')
+def actualizar_vehiculo(request,id):
+    if request.method=='GET':
+        return render(request,'asignacion/update_vehiculo.html')
+    if request.method=='POST':
+        vehiculo=get_object_or_404(Vehiculo,id)
+        matricula_vehiculo=request.POST.get('matricula')
+        marca_vehiculo=request.POST.get('marca')
+        modelo_vehiculo=request.POST.get('modelo')
+
+        vehiculo.matricula_vehiculo=matricula_vehiculo
+        vehiculo.marca_vehiculo=marca_vehiculo
+        vehiculo.modelo_vehiculo=modelo_vehiculo
+        
+        vehiculo.save()
+        return redirect('listar_vehiculos')
+    
+def listar_asignaciones(request):
+    asignaciones = Asignacion.objects.all()
+    return render(request, 'asignacion/listar_asignaciones.html', {'asignaciones': asignaciones})
 
 def crear_asignacion(request):
     if request.method == 'GET':
