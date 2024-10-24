f52fd838fc4dd9b4753f04b65e4eef031413b2f9
Sebastán Holweger <80050935+SHolweger@users.noreply.github.com>
2024-10-24 14:11:46 -0600

    Add files via upload

--- views.py
+++ views.py
@@ -220,3 +220,200 @@ def actualizar_envio(request, id):
         envio.save()  
         messages.success(request, "El estado del envio ha sido actualizado.")
         return redirect('listar_envios')
+
+#UBICACION
+def listar_ubicaciones(request):
+    ubicaciones = Ubicacion.objects.all()
+    return render(request, 'ubicacion/list_ubicacion.html', {'ubicaciones': ubicaciones})
+
+def crear_ubicacion(request):
+    if request.method == 'POST':
+        direccion = request.POST.get('direccion')
+        ciudad = request.POST.get('ciudad')
+        pais = request.POST.get('pais')
+
+        # Crear una nueva instancia del modelo Ubicacion
+        nueva_ubicacion = Ubicacion(
+            direccion=direccion,
+            ciudad=ciudad,
+            pais=pais
+        )
+        nueva_ubicacion.save()
+        messages.success(request, "La ubicación ha sido creada exitosamente.")
+        return redirect('listar_ubicaciones')  
+    return render(request, 'ubicacion/create_ubicacion.html')
+
+def actualizar_ubicacion(request, id):
+    ubicacion = get_object_or_404(Ubicacion, id=id)  
+
+    if request.method == 'POST':
+        ubicacion.direccion = request.POST.get('direccion')
+        ubicacion.ciudad = request.POST.get('ciudad')
+        ubicacion.pais = request.POST.get('pais')
+        ubicacion.save()  # Guardar los cambios
+
+        messages.success(request, "La ubicación ha sido actualizada exitosamente.")
+        return redirect('listar_ubicaciones')
+    return render(request, 'ubicacion/update_ubicacion.html', {'ubicacion': ubicacion})
+
+def eliminar_ubicacion(request, id):
+    ubicacion = get_object_or_404(Ubicacion, id=id)
+
+    if request.method == 'POST':
+        ubicacion.delete()  # Eliminar la ubicación
+        messages.success(request, "La ubicación ha sido eliminada exitosamente.")
+        return redirect('listar_ubicaciones')  # Redirigir a la lista de ubicaciones
+    # Confirmación antes de eliminar
+    return render(request, 'ubicacion/delete_ubicacion.html', {'ubicacion': ubicacion})
+
+
+#FACTURA
+def crear_factura(request):
+    if request.method == 'POST':
+        cliente_id = request.POST.get('cliente')
+        fecha = request.POST.get('fecha')
+        total = request.POST.get('total')
+
+        nueva_factura = Facturacion(
+            cliente_id=cliente_id,
+            fecha=fecha,
+            total=total
+        )
+        nueva_factura.save()
+        messages.success(request, "La factura ha sido creada exitosamente.")
+        return redirect('listar_facturas')
+
+    clientes = Cliente.objects.all()
+    return render(request, 'factura/create_factura.html', {'clientes': clientes})
+
+def listar_facturas(request):
+    facturas = Facturacion.objects.all()
+    return render(request, 'factura/list_facturas.html', {'facturas': facturas})
+
+def actualizar_factura(request, id):
+    factura = get_object_or_404(Facturacion, id=id)
+
+    if request.method == 'POST':
+        factura.cliente_id = request.POST.get('cliente')
+        factura.fecha = request.POST.get('fecha')
+        factura.total = request.POST.get('total')
+        factura.save()
+
+        messages.success(request, "La factura ha sido actualizada exitosamente.")
+        return redirect('listar_facturas')
+
+    clientes = Cliente.objects.all()
+    return render(request, 'factura/update_factura.html', {'factura': factura, 'clientes': clientes})
+
+#FACTURA DETALLE
+def crear_factura_detalle(request):
+    if request.method == 'POST':
+        factura_id = request.POST.get('factura')
+        descripcion = request.POST.get('descripcion')
+        cantidad = request.POST.get('cantidad')
+        precio = request.POST.get('precio')
+
+        nuevo_detalle = FacturacionDetalle(
+            factura_id=factura_id,
+            descripcion=descripcion,
+            cantidad=cantidad,
+            precio=precio
+        )
+        nuevo_detalle.save()
+        messages.success(request, "El detalle de la factura ha sido creado exitosamente.")
+        return redirect('listar_factura_detalle')
+
+    facturas = Facturacion.objects.all()
+    return render(request, 'factura/create_factura_detalle.html', {'facturas': facturas})
+
+def listar_factura_detalle(request):
+    detalles = FacturacionDetalle.objects.all()
+    return render(request, 'factura/list_factura_detalle.html', {'detalles': detalles})
+
+def actualizar_factura_detalle(request, id):
+    detalle = get_object_or_404(FacturacionDetalle, id=id)
+
+    if request.method == 'POST':
+        detalle.factura_id = request.POST.get('factura')
+        detalle.descripcion = request.POST.get('descripcion')
+        detalle.cantidad = request.POST.get('cantidad')
+        detalle.precio = request.POST.get('precio')
+        detalle.save()
+
+        messages.success(request, "El detalle de la factura ha sido actualizado exitosamente.")
+        return redirect('listar_factura_detalle')
+
+    facturas = Facturacion.objects.all()
+    return render(request, 'factura/update_factura_detalle.html', {'detalle': detalle, 'facturas': facturas})
+
+
+#ELIMINAR 
+def eliminar_vehiculo(request, id):
+    vehiculo = get_object_or_404(Vehiculo, id=id)
+    
+    if request.method == 'POST':
+        vehiculo.delete()
+        messages.success(request, "El vehículo ha sido eliminado exitosamente.")
+        return redirect('listar_vehiculos')
+    
+    return render(request, 'asignacion/delete_vehiculo.html', {'vehiculo': vehiculo})
+
+def eliminar_asignacion(request, id):
+    asignacion = get_object_or_404(Asignacion, id=id)
+    
+    if request.method == 'POST':
+        asignacion.delete()
+        messages.success(request, "La asignación ha sido eliminada exitosamente.")
+        return redirect('listar_asignaciones')
+    
+    return render(request, 'asignacion/delete_asignacion.html', {'asignacion': asignacion})
+
+def eliminar_transportista(request, id):
+    transportista = get_object_or_404(Transportista, id=id)
+    
+    if request.method == 'POST':
+        transportista.delete()
+        messages.success(request, "El transportista ha sido eliminado exitosamente.")
+        return redirect('listar_transportistas')
+    
+    return render(request, 'transportista/delete_transportista.html', {'transportista': transportista})
+
+def eliminar_paquete(request, id):
+    paquete = get_object_or_404(Paquete, id=id)
+    
+    if request.method == 'POST':
+        paquete.delete()
+        messages.success(request, "El paquete ha sido eliminado exitosamente.")
+        return redirect('listar_paquetes')
+    
+    return render(request, 'paquete/delete_paquete.html', {'paquete': paquete})
+
+def eliminar_envio(request, id):
+    envio = get_object_or_404(Envio, id=id)
+    
+    if request.method == 'POST':
+        envio.delete()
+        messages.success(request, "El envío ha sido eliminado exitosamente.")
+        return redirect('listar_envios')
+    
+    return render(request, 'envios/delete_envio.html', {'envio': envio})
+
+def eliminar_factura(request, id):
+    factura = get_object_or_404(Facturacion, id=id)
+    
+    if request.method == 'POST':
+        factura.delete()
+        messages.success(request, "La factura ha sido eliminada exitosamente.")
+        return redirect('listar_facturas')
+    
+    return render(request, 'factura/delete_factura.html', {'factura': factura})
+
+def eliminar_factura_detalle(request, id):
+    detalle = get_object_or_404(FacturacionDetalle, id=id)
+    
+    if request.method == 'POST':
+        detalle.delete()
+        messages.success(request, "El detalle de la factura ha sido eliminado exitosamente.")
+        return redirect('listar_factura_detalle')
+    
+    return render(request, 'factura/delete_factura_detalle.html', {'detalle': detalle})
